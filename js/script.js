cc18f8c7be6c75d83ae163a3e342960c4a4c4e35
DavidTheMe <71494143+DavidTheMe@users.noreply.github.com>
2024-10-22 13:03:23 +0200

    finnished calculator

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let numberShown = null;
 let numberNotShown = null;
+let whatToDo = null;
 
 const numberElement = document.getElementById("currentNumText");
 
@@ -21,6 +22,7 @@ function numPressed(num) {
 function restartCalculator() {
     numberShown = null;
     numberNotShown = null;
+    whatToDo = null;
     updateNumber();
 }
 
@@ -28,12 +30,47 @@ function backspace() {
     if (numberShown !== null) {
         // Convert numberShown to a string
         numberShown = String(numberShown);
-        
+
         // Remove the last character
         numberShown = numberShown.substring(0, numberShown.length - 1);
-        
+
         // Convert back to a number, or set to null if it's an empty string
         numberShown = numberShown.length > 0 ? Number(numberShown) : null;
         updateNumber();
     }
+}
+
+function storeShownNumber() {
+    numberNotShown = numberShown;
+    numberShown = null;
+}
+
+function equal() {
+    if (whatToDo == "add") {
+        numberShown = numberNotShown + numberShown;
+    }
+    else if (whatToDo == "sub") {
+        numberShown = numberNotShown - numberShown;
+    } else if (whatToDo == "multi") {
+        numberShown = numberNotShown * numberShown;
+    } else if (whatToDo == "divide") {
+        numberShown = numberNotShown / numberShown;
+    }
+
+    numberNotShown = null;
+    whatToDo = null;
+    updateNumber();
+}
+
+function doButton(doing) {
+    if (numberShown !== null) {
+        if (numberNotShown == null) {
+            storeShownNumber();
+        }
+        else {
+            equal()
+        }
+    }
+    whatToDo = doing;
+    updateNumber();
 }
